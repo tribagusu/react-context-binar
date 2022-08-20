64bd0b9f3e899721e50df6c991a9e35043f196b7
tribagus <tri.bagus21@gmail.com>
2022-08-20 09:41:26 +0700

    init commit

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CardImage from "./CardImage";
+import CardName from "./CardName";
+
+const Card = () => {
+  return (
+    <div>
+      <CardName />
+      <CardImage />
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/CardImage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardImage = () => {
+  return (
+    <div>
+      <img src="https://reqres.in/img/faces/2-image.jpg" alt="" />
+    </div>
+  );
+};
+
+export default CardImage;
